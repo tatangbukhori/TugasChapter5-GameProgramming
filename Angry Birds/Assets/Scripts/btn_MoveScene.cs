33409b7ce452dcd169561840df8064b76fad6814
tatangbukhori <tatangb909@gmail.com>
2021-09-22 21:28:12 +0700

    Tugas Chapter 5

---
+++ Angry Birds/Assets/Scripts/btn_MoveScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btn_MoveScene : MonoBehaviour
+{
+    public string namaScene;
+
+    public void PindahScene()
+    {
+        
+        Scene sceneIni = SceneManager.GetActiveScene();
+
+        if(sceneIni.name != namaScene)
+        {
+            SceneManager.LoadScene(namaScene);
+        }
+    }
+}
+
